6528a0081bd7f3768847eb88b33053e6ef60682f
Nash Brancic <nashfft@gmail.com>
2019-02-23 17:56:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/[aeiou]/)
-  if word == "aeiou"
-    puts true
-  else
-    nil
+  word.match(/[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
